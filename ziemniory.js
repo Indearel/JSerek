1c116c458ab8b1542de54597d3a9bd3f8e084691
Loec <45694592+Loec@users.noreply.github.com>
2019-11-25 16:23:07 +0100

    Create ziemniory.js

---
+++ ziemniory.js
@@ -0,0 +1,16 @@
+let productName = "Ziemniory som",
+    price = 2.50
+    currency = "zloty"
+    weight = "za 1 kg"
+    blackMarket = "Ale mogem je sprzydac pisiont groszy toniej"
+    zapytanie = "Panie, po ile te pyry ?"
+    odpowiedz = "To proszem 2 kylo"
+    bendzieRazem = "To bedziem rozem" 
+    ilePyry = 2 * 2
+
+
+console.log(zapytanie);
+console.log(productName, price, currency, weight);
+console.log(blackMarket);
+console.log(odpowiedz);
+console.log(bendzieRazem, ilePyry, currency);
