f87e42327eeaeb180ddb18e49ef73415d95b4733
Loec <45694592+Loec@users.noreply.github.com>
2019-11-28 15:44:47 +0100

    Add files via upload

---
+++ kup Pan pyry.js
@@ -0,0 +1,26 @@
+//Zasody:
+//Mosz Pon dzisinć PLN-uf na kupno pyr
+//Jak wydosz Pon wincej to Pon przegrywosz
+//
+
+
+
+let Pyry = [
+            "Arielle",
+            "Denar",
+            "Fresco",
+            "Iris",
+            "Lord",
+            "Orlik"
+
+]
+ 
+let jakaPyra = [ Pyry[0], Pyry[2]]
+
+
+console.log("Kup Pany pyry!")
+
+console.log("Wybrol Pon takom pyre:")
+console.log(" " + jakaPyra[0])
+console.log(" " + jakaPyra[1])
+
