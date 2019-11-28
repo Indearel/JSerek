93aaf85cbb7a8be01051cc5c052c985fa69e2505
Loec <45694592+Loec@users.noreply.github.com>
2019-11-28 15:42:42 +0100

    Create ify.js

---
+++ ify.js
@@ -0,0 +1,28 @@
+let Pyry = [
+    "Arielle",
+    "Denar",
+    "Fresco",
+    "Iris",
+    "Lord",
+    "Orlik"
+
+]
+
+if (Pyry === "Arielle") {
+    cena = 2.50;
+}
+else if (Pyry ==="Denar") {
+    cena = 3.00;
+}
+else if (Pyry === "Fresco") {
+    cena = 2.25;
+}
+else if (Pyry === "Iris") {
+    cena = 1.70;
+}
+else if (Pyry === "Lord") {
+    cena = 3.30;
+}
+else if (Pyry === "Orlik") {
+    cena = 2.80;
+}
